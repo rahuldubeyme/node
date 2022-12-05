fc75bff510fee8b184254e1a13696449db60fb18
Rahul <dubeyrahul890@gmail.com>
2022-12-05 21:06:22 +0530

    jwt header

--- app.js
+++ app.js
@@ -49,8 +49,19 @@ app.get('/', function (req, res) {
 
 
 
-
-
+//jwt varify token
+app.use(function(req, res, next) {
+    if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+      jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'randomString', function(err, decode) {
+        if (err) req.user = undefined;
+        req.user = decode;
+        next();
+      });
+    } else {
+      req.user = undefined;
+      next();
+    }
+  });
 
 
 
