7048c5d97a4675c10a19b71a889fa42b2d8306fe
Rahul <dubeyrahul890@gmail.com>
2023-05-11 21:58:14 +0530

    swagger update

--- src/api/app.js
+++ src/api/app.js
@@ -54,10 +54,19 @@ const YAML = require('yamljs');
 
 
 // Load Swagger YAML file
-const swaggerDocument = YAML.load('./src/api/docs/swagger.yaml');
+const indexCommon = YAML.load('./src/api/docs/swagger.yaml');
+// 1
+const lostPeople = YAML.load('./src/api/docs/lost-people.yaml');
+// 2
+const secureChat = YAML.load('./src/api/docs/common.yaml');
+// 3
+const tifinCenter = YAML.load('./src/api/docs/tifin-center.yaml');
 
 // Mount Swagger UI middleware
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/docs/common', swaggerUi.serve, swaggerUi.setup(indexCommon));
+app.use('/docs/tifin-center', swaggerUi.serve, swaggerUi.setup(tifinCenter));
+app.use('/docs/lost-people', swaggerUi.serve, swaggerUi.setup(lostPeople));
+app.use('/docs/secure-chat', swaggerUi.serve, swaggerUi.setup(secureChat));
 
 // routers
 app.use("/", apiRouter);
