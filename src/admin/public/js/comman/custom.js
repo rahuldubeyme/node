69820884aabf6df044cad8bc352a244802d967b3
Rahul <dubeyrahul890@gmail.com>
2023-05-16 23:00:27 +0530

    admin update

--- src/admin/public/js/comman/custom.js
+++ src/admin/public/js/comman/custom.js
@@ -16,4 +16,27 @@ $(this).prop('Counter',0).animate({
 });
 });
 
+
+
+
+
+
+
+
+
+
+  // Get the button element by its ID
+  const button = document.getElementById("myButton");
+
+  // Add a click event listener to the button
+  button.addEventListener("click", function() {
+    // Display the SweetAlert popup
+    Swal.fire({
+      title: 'Hello!',
+      text: 'This is a SweetAlert popup.',
+      icon: 'success',
+      confirmButtonText: 'OK'
+    });
+  });
+
 }); 
